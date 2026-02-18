b586010025f76eb281dcc239c73ca461ca14179c
fotohabilaj <fotohabilaj@gmail.com>
2026-02-18 15:57:13 +0100

    add array  + base html

--- main.js
+++ main.js
@@ -1,2 +1,41 @@
 console.log('virgolette');
 
+const personale = [
+    {
+        nome : 'Marco Bianchi',
+        ruolo : 'Designer',
+        foto : 'male1.png',
+        mail: 'marcobianchi@team.com'
+    },
+    {
+        nome : 'Laura Rossi',
+        ruolo : 'Front-end developer',
+        foto : 'female1.png',
+        mail: 'laurarossi@team.com'
+    },
+    {
+        nome : 'Giorio Verdi',
+        ruolo : 'Back-end developer',
+        foto : 'male2.png',
+        mail: 'giorgioverdi@team.com'
+    },
+    {
+        nome : 'Marta Rossi',
+        ruolo : 'SEO specialist',
+        foto : 'female2.png',
+        mail: 'martarossi@team.com'
+    },
+    {
+        nome : 'Roberto Lorem',
+        ruolo : 'SEO specialist',
+        foto : 'male3.png',
+        mail:  'robertolorem@team.com'
+    },
+    {
+        nome : 'Daniela Amet',
+        ruolo : 'Analyst',
+        foto : 'female3.png',
+        mail: 'danielaamet@team.com'
+    }
+]
+
